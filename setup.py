396b06abfb774b063e0dd9e7c538d7dba5163575
benoitc <bchesneau@gmail.com>
2013-09-07 00:30:15 +0200

    add atomic operations support
    embed the atomic module from @dreid [1]. will be used to handle the atomic lock in
    synchronization functions.
    
    replace the sync module originally done for flower that was working only with
    cpython [2]
    
    [1] https://github.com/dreid/atomiclong
    [2] https://github.com/benoitc/flower/blob/master/flower/core/sync.c

--- setup.py
+++ setup.py
@@ -1,7 +1,20 @@
 import os
-from setuptools import setup, find_packages, Extension
-
 import sys
+
+from setuptools import setup, find_packages, Extension
+from setuptools.command.test import test as TestCommand
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
+
 
 is_pypy = '__pypy__' in sys.builtin_module_names
 py_version = sys.version_info[:2]
@@ -11,7 +24,7 @@ if py_version < (2, 7):
 
 
 
-REQUIREMENTS = []
+REQUIREMENTS = ["cffi"]
 
 if not is_pypy:
     REQUIREMENTS.append("fibers")
@@ -19,7 +32,12 @@ if not is_pypy:
 if py_version == (2, 7):
     REQUIREMENTS.append('futures')
 
-
+try:
+    from flower.core.atomic import ffi
+except ImportError:
+    EXT_MODULES=[]
+else:
+    EXT_MODULES=[ffi.verifier.get_extension()]
 
 CLASSIFIERS = [
     'Development Status :: 4 - Beta',
@@ -43,20 +61,24 @@ with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as f:
     long_description = f.read()
 
 DATA_FILES = [
-        ('flower', ["LICENSE", "MANIFEST.in", "NOTICE", "README.rst",
+        ('offset', ["LICENSE", "MANIFEST.in", "NOTICE", "README.rst",
                         "THANKS"])
         ]
 
 
 setup(name='offset',
-      version='0.1.0',
-      description = 'collection of modules to build distributed and reliable concurrent systems',
-      long_description = long_description,
-      classifiers = CLASSIFIERS,
-      license = 'BSD',
-      url = 'http://github.com/benoitc/offset',
-      author = 'Benoit Chesneau',
-      author_email = 'benoitc@e-engura.org',
-      packages=find_packages(),
-      install_requires = REQUIREMENTS,
-      data_files = DATA_FILES)
+        version='0.1.0',
+        description = 'collection of modules to build distributed and reliable concurrent systems',
+        long_description = long_description,
+        classifiers = CLASSIFIERS,
+        license = 'BSD',
+        url = 'http://github.com/benoitc/offset',
+        author = 'Benoit Chesneau',
+        author_email = 'benoitc@e-engura.org',
+        packages=find_packages(),
+        install_requires = REQUIREMENTS,
+        setup_requires=REQUIREMENTS,
+        tests_require=['pytest'],
+        ext_modules=EXT_MODULES,
+        data_files = DATA_FILES,
+        cmdclass={"test": PyTest})
---
+++ tests/test_atomic.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -
+#
+# This file is part of offset. See the NOTICE for more information.
+# copyright (c) 2013 David Reid under the MIT License.
+
+
+from offset.core.atomic import AtomicLong, ffi, lib
+
+def test_long_add_and_fetch():
+    l = ffi.new('long *', 0)
+    assert lib.long_add_and_fetch(l, 1) == 1
+    assert lib.long_add_and_fetch(l, 10) == 11
+
+def test_long_sub_and_fetch():
+    l = ffi.new('long *', 0)
+    assert lib.long_sub_and_fetch(l, 1) == -1
+    assert lib.long_sub_and_fetch(l, 10) == -11
+
+def test_long_bool_compare_and_swap():
+    l = ffi.new('long *', 0)
+    assert lib.long_bool_compare_and_swap(l, 0, 10) == True
+    assert lib.long_bool_compare_and_swap(l, 1, 20) == False
+
+def test_atomiclong_repr():
+    l = AtomicLong(123456789)
+    assert '<AtomicLong at ' in repr(l)
+    assert '123456789>' in repr(l)
+
+def test_atomiclong_value():
+    l = AtomicLong(0)
+    assert l.value == 0
+    l.value = 10
+    assert l.value == 10
+
+def test_atomiclong_iadd():
+    l = AtomicLong(0)
+    l += 10
+    assert l.value == 10
+
+def test_atomiclong_isub():
+    l = AtomicLong(0)
+    l -= 10
+    assert l.value == -10
+
+def test_atomiclong_eq():
+    l1 = AtomicLong(0)
+    l2 = AtomicLong(1)
+    l3 = AtomicLong(0)
+    assert l1 == 0
+    assert l1 != 1
+    assert not (l2 == 0)
+    assert not (l2 != 1)
+    assert l1 == l3
+    assert not (l1 != l3)
+    assert l1 != l2
+    assert not (l1 == l2)
+
+def test_atomiclong_ordering():
+    l1 = AtomicLong(0)
+    l2 = AtomicLong(1)
+    l3 = AtomicLong(0)
+
+    assert l1 < l2
+    assert l1 <= l2
+    assert l1 <= l3
+    assert l2 > l1
+    assert l2 >= l3
+    assert l2 >= l2
+
+    assert l1 < 1
+    assert l1 <= 0
+    assert l1 <= 1
+    assert l1 > -1
+    assert l1 >= -1
+    assert l1 >= 0
