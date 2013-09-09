1192c35a2923a5185eb8a3d8ced637a6131eb4fc
benoitc <bchesneau@gmail.com>
2013-09-10 00:42:55 +0200

    add once object

---
+++ offset/sync/once.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -
+#
+# This file is part of offset. See the NOTICE for more information.
+
+import functools
+
+from .atomic import AtomicLong
+from .mutex import Mutex
+
+class Once(object):
+    """ Once is an object that will perform exactly one action. """
+
+    def __init__(self):
+        self.m = Mutex()
+        self.done = AtomicLong(0)
+
+    def do(self, func):
+        """ Do calls the function f if and only if the method is being called for the
+
+        ex::
+
+            once = Once
+
+            @once.do
+            def f():
+                return
+
+            # or
+            once.do(f)()
+
+        if once.do(f) is called multiple times, only the first call will invoke
+        f.
+        """
+
+        @functools.wraps(func)
+        def _wrapper(*args, **kwargs):
+            if self.done == 1:
+                return
+
+            self.m.lock()
+            try:
+                if self.done == 0:
+                    func(*args, **kwargs)
+                    self.done.value = 1
+            finally:
+                self.m.unlock()
+        return _wrapper
--- tests/test_sync.py
+++ tests/test_sync.py
@@ -4,9 +4,25 @@
 
 from offset import go, run, maintask, makechan
 from offset.sync.mutex import Mutex
+from offset.sync.once import Once
 
 
-def test_mutext():
+class One(object):
+
+    def __init__(self):
+        self.o = 0
+
+    def incr(self):
+        self.o += 1
+
+    def __eq__(self, other):
+        return self.o == other
+
+    def __ne__(self, other):
+        return not self.o == other
+
+
+def test_Mutex():
 
     def hammer_mutex(m, loops, cdone):
         for i in range(loops):
@@ -27,3 +43,28 @@ def test_mutext():
 
     run()
 
+
+def test_Once():
+
+    def f(o):
+        o.incr()
+
+    def test(once, o, c):
+        once.do(f)(o)
+        assert o == 1
+        c.send(True)
+
+    @maintask
+    def main():
+        c = makechan()
+        once = Once()
+        o = One()
+        for i in range(10):
+            go(test, once, o, c)
+
+        for i in range(10):
+            c.recv()
+
+        assert o == 1
+
+    run()
