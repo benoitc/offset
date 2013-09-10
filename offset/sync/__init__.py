d25297831ca1a1757666f786d6204865607a2e83
benoitc <bchesneau@gmail.com>
2013-09-10 21:50:25 +0200

    make sync functions available via sync.*

---
+++ offset/sync/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -
+#
+# This file is part of offset. See the NOTICE for more information.
+
+from .sema import Semaphore
+from .mutex import Mutex
+from .once import Once
+from .cond import Cond
+from .rwmutex import RWMutex
+from .waitgroup import WaitGroup
