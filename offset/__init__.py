6b32ddf59db6b2bb1865d37e9e41c9b2308a7e69
benoitc <bchesneau@gmail.com>
2013-09-10 21:48:18 +0200

    add version info

--- offset/__init__.py
+++ offset/__init__.py
@@ -2,6 +2,9 @@
 #
 # This file is part of offset. See the NOTICE for more information.
 
+version_info = (0, 1, 0)
+__version__ = ".".join([str(v) for v in version_info])
+
 # scheduler functions
 from .core import go, run, gosched, maintask
 
